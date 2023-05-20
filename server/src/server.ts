a951e2fd8b6ec4333976f5469d4700ecf699af44
Rafael Rodrigues <rafael-rafa2.1@hotmail.com>
2023-05-20 20:56:24 -0300

    Aula 4

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,23 @@
 import "dotenv/config";
 
 import fastify from "fastify";
+import multipart from "@fastify/multipart";
 import cors from "@fastify/cors";
 import jwt from "@fastify/jwt";
+import { resolve } from "node:path";
 
 import { memoriesRoutes } from "./routes/memories";
 import { authRoutes } from "./routes/auth";
+import { uploadRoutes } from "./routes/upload";
 
 const app = fastify();
+
+app.register(multipart);
+
+app.register(require("@fastify/static"), {
+  root: resolve(__dirname, "../uploads"),
+  prefix: "/uploads",
+});
 
 app.register(cors, {
   origin: true,
@@ -17,8 +27,9 @@ app.register(jwt, {
   secret: "spacetime",
 });
 
-app.register(memoriesRoutes);
 app.register(authRoutes);
+app.register(uploadRoutes);
+app.register(memoriesRoutes);
 
 app
   .listen({
--- web/src/components/Profile.tsx
+++ web/src/components/Profile.tsx
@@ -15,10 +15,13 @@ export function Profile() {
       />
 
       <p className="max-w-[140px] text-sm leading-snug">
-        {name}{' '}
-        <a href="#" className="block text-red-400 hover:text-red-300">
+        {name}
+        <a
+          href="/api/auth/logout"
+          className="block text-red-400 hover:text-red-300"
+        >
           Quero sair
-        </a>{' '}
+        </a>
       </p>
     </div>
   )
