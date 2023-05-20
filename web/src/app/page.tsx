1928b3f2ab9df7b8a1cb8cf6cd6c5ffd0f95295e
Rafael Rodrigues <rafael-rafa2.1@hotmail.com>
2023-05-20 10:12:57 -0300

    aula 1

---
+++ web/src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="h-screen bg-zinc-900 text-zinc-50">
+      <h1>Sua cápsula do tempo</h1>
+      <h1 className="font-alt">Sua cápsula do tempo</h1>
+    </div>
+  )
+}
